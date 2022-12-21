56c36dd7dcaa68c3d502d241baa1ab2865ef0997
Ruman Maharjan <rumanmaharjan770@gmail.com>
2022-12-21 10:22:29 +0545

    icons changes

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import logo from './logo.svg';
 import './App.css';
 import Stock from './Stock';
 import {STOCKS} from "./constants";
+import Counter from './Counter';
+
 function App() {
   const person = {
     name: 'test',
@@ -13,6 +15,7 @@ function App() {
   const scores = [20,23,25,23,22,12];
   return (
     <div className="App">
+      <Counter /> 
       {STOCKS.map((stock)=>(
         <Stock 
         key={stock.securityId} securityName= {stock.securityName} symbol={stock.symbol} lastTradePrice={stock.lastTradePrice}/>
--- src/Counter.js
+++ src/Counter.js
@@ -1,5 +1,17 @@
+
 import React from "react";
-const Counter = () =>{
-    return <div></div>
+import { BsPlusCircle } from "react-icons/bs";
+import { BiMinusCircle } from "react-icons/bi";
+const Counter = () => {
+  const count = 0;
+  // state to be used later
+  return (
+    <div>
+      <BsPlusCircle color={"green"} size={32} on />
+      <h1>{count}</h1>
+      <BiMinusCircle color={"red"} size={32} />
+    </div>
+  );
 };
+
 export default Counter;
