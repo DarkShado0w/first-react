8e851172312097d03feaf9556a00df7e393e0c9d
Ruman Maharjan <rumanmaharjan770@gmail.com>
2022-12-21 12:03:11 +0545

    changes 9

--- src/Stock.js
+++ src/Stock.js
@@ -1,14 +1,14 @@
 import React from "react";
 
-const Stock = ({securityName, symbol ,lastTradePrice}) => {
-    // console.log(props);
+const Stock = ({securityName, symbol ,lastTradePrice, ...rest }) => {
+  console.log(rest,"in stock");
     return (
         <div>
             <h1>{securityName}</h1>
             <h2>{symbol}</h2>
             <h2>{lastTradePrice}</h2>
         </div>
-    )
-}
+    );
+};
    
 export default Stock;
